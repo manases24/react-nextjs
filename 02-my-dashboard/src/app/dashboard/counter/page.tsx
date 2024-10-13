d328a36868e01673c088a580579d0692179c5b8a
manases24 <hugo.orlando2050@gmail.com>
2024-10-13 12:58:44 -0300

    shopping cart components is added

--- 02-my-dashboard/src/app/dashboard/counter/page.tsx
+++ 02-my-dashboard/src/app/dashboard/counter/page.tsx
@@ -1,7 +1,15 @@
+import { CartCounter } from "@/app/shopping-cart/components";
+
+export const metadata = {
+  title: "Shopping Cart",
+  description: "Un simple contador",
+};
+
 export default function CounterPage() {
   return (
-    <div>
-      <h1>Counter Page</h1>
+    <div className="flex flex-col items-center justify-center w-full h-full">
+      <span>Productos en el carrito de compras</span>
+      <CartCounter value={10} />
     </div>
   );
 }
