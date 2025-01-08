9d14066f8bf6806dab46dbe1b1903216dcca3373
manases <hugo.orlando2050@gmail.com>
2025-01-08 16:20:52 -0300

    next + rest api

---
+++ 03-admin-todos/src/components/Sidebar.tsx
@@ -0,0 +1,77 @@
+import Image from "next/image";
+import Link from "next/link";
+import { CiLogout } from "react-icons/ci";
+
+import {
+  IoCalendarOutline,
+  IoCheckboxOutline,
+  IoListOutline,
+} from "react-icons/io5";
+import { SidebarItem } from "./SidebarItem";
+
+const menuItems = [
+  {
+    icon: <IoCalendarOutline />,
+    title: "Dashboard",
+    path: "/dashboard",
+  },
+  {
+    icon: <IoCheckboxOutline />,
+    title: "Rest TODOS",
+    path: "/dashboard/rest-todos",
+  },
+  {
+    icon: <IoListOutline />,
+    title: "Server Actions",
+    path: "/dashboard/server-todos",
+  },
+];
+
+export const Sidebar = () => {
+  return (
+    <aside className="ml-[-100%] fixed z-10 top-0 pb-3 px-6 w-full flex flex-col justify-between h-screen border-r bg-white transition duration-300 md:w-4/12 lg:ml-0 lg:w-[25%] xl:w-[20%] 2xl:w-[15%]">
+      <div>
+        <div className="-mx-6 px-6 py-4">
+          {/* TODO: Next/Link hacia dashboard */}
+          <Link href="#" title="home">
+            {/* Next/Image */}
+            <Image
+              src="https://images.unsplash.com/photo-1736288002606-9db16875f180?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzMnx8fGVufDB8fHx8fA%3D%3D"
+              className="w-10 h-10 rounded-full"
+              alt="tailus logo"
+              width={150}
+              height={150}
+            />
+          </Link>
+        </div>
+
+        <div className="mt-8 text-center">
+          <Image
+            src="https://images.unsplash.com/photo-1542909168-82c3e7fdca5c"
+            width={150}
+            height={150}
+            alt=""
+            className="w-10 h-10 m-auto rounded-full object-cover lg:w-28 lg:h-28"
+          />
+          <h5 className="hidden mt-4 text-xl font-semibold text-gray-600 lg:block">
+            John Doe
+          </h5>
+          <span className="hidden text-gray-400 lg:block">Admin</span>
+        </div>
+
+        <ul className="space-y-2 tracking-wide mt-8">
+          {menuItems.map((item) => (
+            <SidebarItem key={item.path} {...item} />
+          ))}
+        </ul>
+      </div>
+
+      <div className="px-6 -mx-6 pt-4 flex justify-between items-center border-t">
+        <button className="px-4 py-3 flex items-center space-x-4 rounded-md text-gray-600 group">
+          <CiLogout />
+          <span className="group-hover:text-gray-700">Logout</span>
+        </button>
+      </div>
+    </aside>
+  );
+};
