fd65e8aa692f557e503ab41cd94a3992e58cc391
manases <hugo.orlando2050@gmail.com>
2025-01-08 17:50:52 -0300

    next + rest api

--- 03-admin-todos/src/todos/components/NewTodo.tsx
+++ 03-admin-todos/src/todos/components/NewTodo.tsx
@@ -25,28 +25,26 @@ export const NewTodo = () => {
   };
 
   return (
-    <form onSubmit={onSubmit} className="flex w-full">
+    <form onSubmit={onSubmit} className="flex w-full gap-2">
       <input
         type="text"
         onChange={(e) => setDescription(e.target.value)}
         value={description}
-        className="w-6/12 -ml-10 pl-3 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-sky-500 transition-all"
+        className="flex-1 pl-3 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-sky-500 transition-all"
         placeholder="¿Qué necesita ser hecho?"
       />
 
       <button
         type="submit"
-        className="flex items-center justify-center rounded ml-2 bg-sky-500 p-2 text-white hover:bg-sky-700 transition-all"
+        className="flex items-center justify-center rounded bg-sky-500 px-4 py-2 text-slate-900 hover:bg-sky-700 transition-all"
       >
         Crear
       </button>
 
-      <span className="flex flex-1"></span>
-
       <button
         onClick={() => deleteCompleted()}
         type="button"
-        className="flex items-center justify-center rounded ml-2 bg-red-400 p-2 text-white hover:bg-red-700 transition-all"
+        className="flex items-center justify-center rounded px-4 py-2 text-slate-900 hover:bg-red-700 transition-all"
       >
         <IoTrashOutline />
         <span className="ml-2">Borrar completados</span>
