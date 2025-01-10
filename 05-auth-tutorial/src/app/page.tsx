48e0c0d08f35cbf02af45e20c12cc85a69a04f52
manases <hugo.orlando2050@gmail.com>
2025-01-10 16:32:12 -0300

    auth tutorial

---
+++ 05-auth-tutorial/src/app/page.tsx
@@ -0,0 +1,6 @@
+import { Button } from "@/components/ui/button";
+
+// app/page.tsx es la pagina principal de la app (root page)
+export default function Home() {
+  return <Button size="lg">Click me</Button>;
+}
