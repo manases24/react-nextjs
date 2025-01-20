003e2e0a30fcefdf93f44faae3ff7793d6ad3672
manases <hugo.orlando2050@gmail.com>
2025-01-20 15:02:22 -0300

    cart store

--- 04-teslo-shop/src/app/(shop)/product/[slug]/ui/AddToCart.tsx
+++ 04-teslo-shop/src/app/(shop)/product/[slug]/ui/AddToCart.tsx
@@ -1,7 +1,8 @@
 "use client";
 
 import { QuantitySelector, SizeSelector } from "@/components";
-import { Product, Size } from "@/interfaces";
+import { CartProduct, Product, Size } from "@/interfaces";
+import { useCartStore } from "@/store/cart/cart-store";
 import { useState } from "react";
 
 interface Props {
@@ -9,6 +10,7 @@ interface Props {
 }
 
 export const AddToCart = ({ product }: Props) => {
+  const addProductToCart = useCartStore((state) => state.addProductTocart);
   const [size, setSize] = useState<Size | undefined>();
   const [quantity, setQuantity] = useState<number>(1);
   const [posted, setPosted] = useState<boolean>(false);
@@ -16,13 +18,29 @@ export const AddToCart = ({ product }: Props) => {
   const addToCart = () => {
     setPosted(true);
     if (!size) return;
+
+    // add to cart
+    const cartProduct: CartProduct = {
+      id: product.id,
+      slug: product.slug,
+      title: product.title,
+      price: product.price,
+      quantity: quantity,
+      size: size,
+      image: product.images[0],
+    };
+
+    addProductToCart(cartProduct);
+    setPosted(false);
+    setQuantity(1);
+    setSize(undefined);
   };
 
   return (
     <>
       {posted && !size && (
         <span className="mt-2 text-red-700 text-sm bg-red-100 border-l-4 border-red-500 px-4 py-2 rounded-lg shadow-md">
-          Seleccionar el talle por favor
+          Seleccionar el talle por favor*
         </span>
       )}
 
